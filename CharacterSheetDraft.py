13c49b2d183cf388d9d814971871a6faf614e576
Yuri-Bles <553227@student.fontys.nl>
2025-09-11 10:08:29 +0200

    Added method to add stat in BLL

---
+++ CharacterSheetDraft.py
@@ -0,0 +1,10 @@
+
+class CharacterSheetDraft:
+    def __init__(self, stats):
+        self.stats = stats
+
+        #The values of a stat are in order: Name, Default Value, Minimum Value, Maximum Value
+    stats = list([])
+
+    def addStat(self, name: str, defaultValue: int, minimumValue: int, maximumValue: int):
+        self.stats.append([name, defaultValue, minimumValue, maximumValue])
--- CombatCreator2BLL.py
+++ CombatCreator2BLL.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from CharacterSheetDraft import CharacterSheetDraft
+
+
+newCharacter = CharacterSheetDraft([
+        ["Health Points", 17, 0, 20],
+        ])
+
+print(newCharacter.stats)
+
+newCharacter.addStat("Mana", 20, 0, None)
+
+print(newCharacter.stats)
