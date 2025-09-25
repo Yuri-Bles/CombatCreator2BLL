68f6b2df4ca3a29a2f66de4b2a060e9cb99a4108
Yuri-Bles <553227@student.fontys.nl>
2025-09-25 11:42:58 +0200

    CI Test 6

--- CombatCreator5TDAL/CombatCreator5TDAL.py
+++ CombatCreator5TDAL/CombatCreator5TDAL.py
@@ -1,1 +1,14 @@
+# test_my_module.py
+import unittest
+from my_module import add
 
+class TestAddFunction(unittest.TestCase):
+    
+    def test_add_positive_numbers(self):
+        self.assertEqual(5, 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(2, -3)
+
+if __name__ == '__main__':
+    unittest.main()
