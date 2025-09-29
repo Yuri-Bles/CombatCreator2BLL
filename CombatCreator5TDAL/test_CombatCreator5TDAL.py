8917588af8513b2c225a9c3a246014ea3bfdd454
Yuri-Bles <553227@student.fontys.nl>
2025-09-29 10:14:22 +0200

    CI Test 9

--- CombatCreator5TDAL/test_CombatCreator5TDAL.py
+++ CombatCreator5TDAL/test_CombatCreator5TDAL.py
@@ -7,8 +7,5 @@ class TestAddFunction(unittest.TestCase):
     def test_add_numbers_true(self):
         self.assertEqual(5, 5)
 
-    def test_add_numbers_false(self):
-        self.assertEqual(2, 2)
-
 if __name__ == '__main__':
     unittest.main()
