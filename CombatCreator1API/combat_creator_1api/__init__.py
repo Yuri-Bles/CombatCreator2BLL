24cf83a1452012c56667808da357f5b428ecd155
Yuri-Bles <553227@student.fontys.nl>
2025-10-01 15:07:15 +0200

    Deleted everything for a restart with Pylint

--- CombatCreator1API/combat_creator_1api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Initialize the Flask application package.
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
--- CombatCreator5TDAL/test_CombatCreator5TDAL.py
+++
@@ -1,7 +0,0 @@
-# test_my_module.py
-import unittest
-
-class TestAddFunction(unittest.TestCase):
-    
-    def test_add_numbers_true(self):
-        self.assertEqual(6, 6)
